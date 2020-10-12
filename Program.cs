ad11140c76d30ab9de9d9367cd7ffa2abd1ed91c
Henke <70940165+92Hank@users.noreply.github.com>
2020-10-12 22:08:31 +0200

    FirstCommit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace NewReadAndWrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadAndWrite input = new ReadAndWrite();
+
+
+            string isRunning = "0";
+            while (isRunning != "q")
+            {
+                Console.Clear();
+
+                Console.WriteLine("\t _____________________");
+                Console.WriteLine("\t |----TheNameList----|");
+                Console.WriteLine("\t |[1]. Enter name    |");
+                Console.WriteLine("\t |[2]. List all names|");
+                Console.WriteLine("\t |[Q]. for Quit      |");
+                Console.WriteLine("\t |---Select option---|");
+                Console.WriteLine("\t |-------1 - 2-------|");
+                Console.WriteLine("\t |___________________|");
+
+                isRunning = Console.ReadLine();
+                Console.WriteLine();
+
+                switch (isRunning.ToUpper())
+                {
+                    case "1":
+                        input.WriteToFile();
+                        input.ReadToFile();
+                        break;
+                    case "2":
+                        input.ListNames();
+                        break;
+                    case "Q":
+                        Console.WriteLine("\t The program ends here...");
+                        Environment.Exit(-1);
+                        break;
+                    default:
+                        Console.WriteLine("\t This option was incorrect!");
+                        Console.WriteLine("\t Press enter to continue");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ReadAndWrite.cs
@@ -0,0 +1,55 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace NewReadAndWrite
+{
+    public class ReadAndWrite
+    {
+        public List<string> names = new List<string>();
+        public void WriteToFile()
+        {
+            Console.WriteLine("\t Enter a name you want to add: ");
+            string name = Console.ReadLine();
+
+            names.Add(name);
+            Console.WriteLine("\t You entered: ");
+            Console.WriteLine("\t " + name);
+
+            using (var writer = File.AppendText($"name.txt"))
+            {
+                writer.WriteLine(name);
+            }
+            Console.ReadLine();
+        }
+
+        public void ReadToFile()
+        {
+            using (var reader = File.OpenText($"name.txt"))
+            {
+                var line = reader.ReadLine();
+                while (line != null)
+                {
+                    var savedName = line;
+                    names.Add(savedName);
+                    line = reader.ReadLine();
+                }
+
+                Console.WriteLine(line);
+            }
+        }
+
+        public void ListNames()
+        {
+            Console.WriteLine("\t All names from namn2.txt:");
+            Console.WriteLine();
+
+            foreach (var allNames in names)
+            {
+                Console.WriteLine("\t " + allNames);
+            }
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
+        }
+    }
+}
